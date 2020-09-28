e19cfec471892e53e80468f06389044a020ffb4b
Sameer Puri <sameer@users.noreply.github.com>
2020-09-27 21:03:56 -0400

    initial commit under AGPLv3

---
+++ src/ast.rs
@@ -0,0 +1,249 @@
+use derive_more::Display;
+use rug::{Float, Integer as Int};
+
+use std::fmt::{Debug, Display, Formatter, Result};
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Comma<T: Clone + Display + Debug + PartialEq>(pub Vec<T>);
+
+impl<T: Clone + Display + Debug + PartialEq> Display for Comma<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        if let Some(item) = self.0.first() {
+            write!(f, "{}", item)?;
+        }
+        for item in self.0.iter().skip(1) {
+            write!(f, ",{}", item)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Item {
+    /// const x: u3 = 0b000;
+    Const {
+        ident: Ident,
+        ty: Type,
+        expr: Expr,
+    },
+    /// impl X {
+    /// }
+    Impl,
+    Entity,
+    /// A special type useful for keeping track of state,
+    /// sending named commands, etc.
+    /// A discriminator is inferred according to the enum size
+    /// and the backing register will be as large as the largest variant.
+    Enum,
+    /// Functions can have a self ref
+    Fn,
+    /// Collection of behaviors that a type satisfies
+    /// Entities cannot implement traits, but types can
+    Trait,
+    /// Import entities, functions, etc. from other modules
+    Use,
+    /// Don't repeat yourself
+    Macro,
+    /// entity Sawtooth16Bit = Sawtooth<u16>;
+    EntityAlias,
+    Mod,
+}
+
+#[derive(Clone, Debug, PartialEq, Display)]
+pub enum Type {
+    #[display(fmt = "[{}; {}]", _0, _1)]
+    Array(Box<Type>, Box<Lit>),
+    #[display(fmt = "{}", _0)]
+    Path(Path),
+    #[display(
+        fmt = "fn({}){}",
+        _0,
+        "_1.as_ref().map(|x| format!(\" -> ({})\", x)).unwrap_or_default()"
+    )]
+    Fn(Comma<Type>, Option<Box<Type>>),
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Display)]
+#[display(fmt = "{}", inner)]
+pub struct Ident {
+    pub inner: String,
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Path {
+    pub leading_colon: Option<()>,
+    pub segments: Vec<Ident>,
+}
+
+impl Display for Path {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        for (i, seg) in self.segments.iter().enumerate() {
+            write!(
+                f,
+                "{}{}",
+                if i != 0 || self.leading_colon.is_some() {
+                    "::"
+                } else {
+                    ""
+                },
+                seg
+            )?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Display)]
+pub enum Expr {
+    // Block(Block),
+    #[display(fmt = "({} {})", _0, _1)]
+    Unary(UnOp, Box<Expr>),
+    #[display(fmt = "({} {} {})", _1, _0, _2)]
+    Binary(Box<Expr>, BinOp, Box<Expr>),
+    #[display(fmt = "({} {} {})", _1, _0, _2)]
+    Assign(Box<Expr>, AssOp, Box<Expr>),
+    #[display(fmt = "[{}; {}]", _0, _1)]
+    Repeat(Box<Expr>, Box<Expr>),
+    /// From, to, closed-ness
+    #[display(
+        fmt = "{}..{}{}",
+        "_0.as_ref().map(|x| format!(\"{}\", x)).unwrap_or_default()",
+        "if *_2 { \"=\" } else { \"\" }",
+        "_1.as_ref().map(|x| format!(\"{}\", x)).unwrap_or_default()"
+    )]
+    Range(Option<Box<Expr>>, Option<Box<Expr>>, bool),
+    #[display(fmt = "{}", _0)]
+    Concat(Comma<Expr>),
+    #[display(fmt = "{}", _0)]
+    Path(Path),
+    #[display(fmt = "{}", _0)]
+    Lit(Lit),
+    #[display(fmt = "{}.{}", _0, _1)]
+    Field(Box<Expr>, Member),
+    #[display(fmt = "{}({})", _0, _1)]
+    Call(Path, Comma<Expr>),
+    #[display(fmt = "{}.{}({})", _0, _1, _2)]
+    MethodCall(Box<Expr>, Ident, Comma<Expr>),
+    #[display(fmt = "{}[{}]", _0, _1)]
+    Index(Box<Expr>, Box<Expr>),
+    #[display(fmt = "[{}]", _0)]
+    Array(Comma<Expr>),
+    #[display(fmt = "(as {} {})", _0, _1)]
+    Cast(Box<Expr>, Box<Type>),
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Display)]
+pub enum Member {
+    #[display(fmt = "{}", _0)]
+    Named(Ident),
+    #[display(fmt = "{}", _0)]
+    Unnamed(u32),
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Display)]
+pub enum BinOp {
+    #[display(fmt = "+")]
+    Add,
+    #[display(fmt = "-")]
+    Sub,
+    #[display(fmt = "*")]
+    Mul,
+    #[display(fmt = "/")]
+    Div,
+    #[display(fmt = "%")]
+    Rem,
+    #[display(fmt = "&&")]
+    And,
+    #[display(fmt = "||")]
+    Or,
+    #[display(fmt = "^")]
+    BitXor,
+    #[display(fmt = "&")]
+    BitAnd,
+    #[display(fmt = "|")]
+    BitOr,
+    #[display(fmt = "<<")]
+    Shl,
+    #[display(fmt = ">>")]
+    Shr,
+    #[display(fmt = "==")]
+    Eq,
+    #[display(fmt = "<")]
+    Lt,
+    #[display(fmt = "<=")]
+    Le,
+    #[display(fmt = "!=")]
+    Ne,
+    #[display(fmt = ">=")]
+    Ge,
+    #[display(fmt = ">")]
+    Gt,
+    #[display(fmt = "**")]
+    Exp,
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Display)]
+pub enum UnOp {
+    #[display(fmt = "!")]
+    Not,
+    #[display(fmt = "-")]
+    Neg,
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Display)]
+pub enum AssOp {
+    #[display(fmt = "=")]
+    Eq,
+    #[display(fmt = "+=")]
+    AddEq,
+    #[display(fmt = "-=")]
+    SubEq,
+    #[display(fmt = "*=")]
+    MulEq,
+    #[display(fmt = "/=")]
+    DivEq,
+    #[display(fmt = "%=")]
+    RemEq,
+    #[display(fmt = "^=")]
+    BitXorEq,
+    #[display(fmt = "&=")]
+    BitAndEq,
+    #[display(fmt = "|=")]
+    BitOrEq,
+    #[display(fmt = "<<=")]
+    ShlEq,
+    #[display(fmt = ">>=")]
+    ShrEq,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Stmt {
+    Let(Let),
+    Item(Item),
+    Expr { inner: Expr, semi: bool },
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum Pattern {}
+
+/// let binding
+#[derive(Clone, Debug, PartialEq)]
+pub struct Let {
+    pat: Pattern,
+    ty: Option<Type>,
+    init: Box<Expr>,
+}
+
+#[derive(Clone, Debug, PartialEq, Display)]
+pub enum Lit {
+    #[display(fmt = "{}", val)]
+    Int {
+        val: Int,
+        /// i.e. i32
+        type_hint: Option<(char, usize)>,
+    },
+    #[display(fmt = "{}", val)]
+    Float { val: Float },
+}
+
+pub type Block = Vec<Stmt>;
---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+#[macro_use]
+extern crate lalrpop_util;
+
+mod ast;
+
+lalrpop_util::lalrpop_mod!(pub rhdl);
+
+#[cfg(test)]
+mod tests {
+    use super::{ast::*, rhdl::*};
+
+    #[test]
+    fn int_parser() {
+        macro_rules! parse {
+            ($($($input: expr),+ => $expected: expr),+) => {
+                $(
+                    $(
+                        assert_eq!(IntParser::new().parse($input), Ok(Lit::Int {
+                            val: rug::Integer::from($expected),
+                            type_hint: None,
+                        }));
+                    )+
+                )+
+            };
+        }
+        parse!(
+            "123", "1_2_3" => 123,
+            "0b101", "0b1_01" => 0b101,
+            "0o72" => 0o72,
+            "0d1_2_3" => 123,
+            "0xB105F00D", "0xB105_F00D" => 0xB105F00Du32,
+            "36#HELLO_THERE" => 1767707662651898u64
+        );
+        assert!(IntParser::new().parse("?").is_err());
+    }
+
+    #[test]
+    fn float_parser() {
+        macro_rules! parse {
+            ($($($input: expr),+ => ($prec: expr, $expected: expr)),+) => {
+                $(
+                    $(
+                        let input = $input;
+                        let res = FloatParser::new().parse(&input);
+                        let expected = Ok(Lit::Float {
+                            val: rug::Float::with_val($prec, $expected),
+                        });
+                        assert_eq!(res, expected);
+                    )+
+                )+
+            };
+        }
+        parse!(
+            format!("{}f64", std::f64::consts::E) => (64, std::f64::consts::E),
+            format!("{}f32", std::f32::consts::PI) => (64, std::f32::consts::PI),
+            "0f16" => (16, 0),
+            "0f32" => (32, 0),
+            "0f64" => (64, 0),
+            "0f128" => (128, 0),
+            "0f256" => (256, 0)
+        );
+        assert!(FloatParser::new().parse("?").is_err());
+    }
+
+    #[test]
+    fn expr_parser() {
+        macro_rules! parse {
+            ($($($input: expr),+ => $expected: expr),+) => {
+                $(
+                    $(
+                        assert_eq!(ExprParser::new().parse($input).map(|output| format!("{}", output)), Ok($expected.to_string()));
+                    )+
+                )+
+            };
+        }
+        parse!(
+            "a = b == c | d ^ e & f << g + h * i ** !j" => "(= a (== b (| c (^ d (& e (<< f (+ g (* h (** i (! j))))))))))"
+        );
+    }
+}
